cf9b7151913bc0361e44c5c5b14f6c79950a92fd
narawitpi-sketch <narawit.pi@nsru.ac.th>
2025-12-16 15:48:00 +0700

    Update 15:47 16/12/2568

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 // src/config/constants.ts
 export const APP_ID = 'smart-classroom';
-export const ALLOWED_ADMIN_EMAILS = ['narawit.pi@nsru.ac.th,nareupron.s@nsru.ac.th,phatchayan.t@nsru.ac.th,ratchadaporn.l@nsru.ac.th'];
+export const ALLOWED_ADMIN_EMAILS = ['narawit.pi@nsru.ac.th','nareupron.s@nsru.ac.th','phatchayan.t@nsru.ac.th','ratchadaporn.l@nsru.ac.th'];
 export const LINE_CHANNEL_ACCESS_TOKEN = "GA3r5ViM4lH1TYGzllT9XKErXn2MlxUKBq8F9c4R/SIeAqHMrKKaGwopC9dcv1vNdcb2/g9383YGFjvMUW72bqHVaqjYUpHPbAYHv+a8glAc4wWda5c0dQyP+IjS4TAHSvVt0EW3v/IdSX4xfknHNAdB04t89/1O/w1cDnyilFU="; 
 export const LINE_GROUP_ID = "C8d92d6c426766edb968dabcb780d4c39";
 
